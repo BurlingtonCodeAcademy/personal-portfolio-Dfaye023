354d223fac04d397d60595e43cd62d9889c008f0
Dfaye023 <70286830+Dfaye023@users.noreply.github.com>
2020-10-19 09:02:05 -0400

    final portfolio

---
+++ client/src/projects.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+
+
+function Projects() {
+    return (
+      <div class="projects">
+        <h1>Projects Page</h1>
+
+          <h2>GitHub User: Dfaye023</h2>
+          <a id='repolink' href='https://github.com/Dfaye023?tab=repositories'>Link to GitHub Repository</a>
+          <a href='https://github.com/BurlingtonCodeAcademy/guess-the-number-Dfaye023.git'>Guess the Number Game</a>
+          <a href='https://github.com/BurlingtonCodeAcademy/remock-Dfaye023.git'>Remock Project</a>
+          <a href='https://github.com/BurlingtonCodeAcademy/tic-tac-toe-hope-dakota.git'>Tic-Tac-Toe Game</a>
+          <a href='https://github.com/BurlingtonCodeAcademy/zorkington-dakota-todd.git'>Zorkington Text Adventure Game</a>
+       
+      </div>
+    );
+  }
+
+
+
+
+
+
+
+export default Projects;
---
+++ client/src/work.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import './App.css';
+
+
+function Work() {
+  return (
+    <div class="work">
+      <h1>Work History</h1>
+      <h3>eCommerce Sales<br />
+          Replicarz<br />
+          Rutland, VT<br />
+          January 2015 to May 2020</h3>
+          <p>
+        Learned and gained experience using MOM (Multichannel Order Manager) to process daily internet orders.<br />
+        Tasked with meeting daily listing/sales goals in a fast-paced work environment.<br />
+        Frequently used high attention to detail to identify any product flaws.<br />
+        Gained an understanding of the eBay sales cycle.</p>
+      <h3>Insurance Sales Agent<br />
+        AAA Northern New England <br />
+        Montpelier, VT<br />
+        January 2011 to January 2015</h3>
+        <p>Tasked with selling property & casualty insurance for home & automobiles in the state of Vermont.<br />
+Experience successfully cold-calling potential customers to establish relationship.<br />
+Used customer service skills to maintain positive client relationships.<br />
+Gained certification to sell property & casualty insurance in state of Vermont.</p>
+      <a id='resume' href='https://my.indeed.com/p/dakotaf-al4hlkx' download>Resume</a>
+    
+    
+    
+    </div>
+    
+  
+  );
+}
+
+
+
+
+
+export default Work;
